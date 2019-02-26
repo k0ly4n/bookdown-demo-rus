1cd8a415f13de53bde8de49149730024b5edcceb
Nikolay Strelkov <n.strelkov@gmail.com>
2019-02-26 18:27:48 +0300

    fix _build.sh

--- _build.sh
+++ _build.sh
@@ -4,5 +4,8 @@ set -ev
 
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_document2')"
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::html_document2')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::word_document2')"
 
